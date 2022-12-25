c36595ad310263069888676439e0e5a830e5c051
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-25 20:37:00 -0300

    error solved

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Weather = ({ data }) => {
   console.log(data);
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[88vh] m-auto p-4 text-gray-300 z-10">
       {/* Top side */}
       <div className="relative flex justify-between pt-12">
         <div className="flex flex-col items-center">

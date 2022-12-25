1e08f34e9761ae7845217ad08ee30d15d95ed386
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-25 20:31:52 -0300

    page maded, still a bug in mobile

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -5,39 +5,59 @@ const Weather = ({ data }) => {
   console.log(data);
   return (
     <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
-    {/* Top side */}
+      {/* Top side */}
       <div className="relative flex justify-between pt-12">
         <div className="flex flex-col items-center">
-        <Image
+          <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-            alt='/'
-            width='100'
-            height='100'
+            alt="/"
+            width="100"
+            height="100"
           />
-          <p className='text-2xl'>{data.weather[0].main}</p>
-          <p>Clouds : {data.clouds.all}%</p>
+          <p className="text-2xl">{data.weather[0].main}</p>
         </div>
         <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;</p>
       </div>
-    {/* Bottom side */}
-    <div>
-        <p>Weather in {data.name}</p>
-        <p>Coord: {data.coord.lon},{data.coord.lat}</p>
-        <div>
-            <div>
-                <p>Feels Like</p>
-                <p>{data.main.feels_like.toFixed(0)}&#176;</p>
-            </div>
-            <div>
-                <p>Humidity</p>
-                <p>{data.main.humidity}%</p>
-            </div>
-            <div>
-                <p>Wind</p>
-                <p>{data.wind.speed.toFixed(0)} MPH </p>
-            </div>
+      {/* Bottom side */}
+      <div className="bg-black/50 relative p-10 rounded-md">
+        <p className="text-2xl text-center pb-6">Weather in {data.name}</p>
+        <div className="flex justify-between text-center">
+          <div>
+            <p className="text-xl">Feels Like</p>
+            <p className="font-bold text-2xl">
+              {data.main.feels_like.toFixed(0)}&#176;
+            </p>
+          </div>
+          <div>
+            <p className="text-xl">Max Temp</p>
+            <p className="font-bold text-2xl">{data.main.temp_max}</p>
+          </div>
+          <div>
+            <p className="text-xl">Humidity</p>
+            <p className="font-bold text-2xl">{data.main.humidity}%</p>
+          </div>
         </div>
-    </div>
+      </div>
+      <div className="bg-black/50 relative p-6 rounded-md">
+        <div className="flex justify-between text-center">
+          <div>
+            <p className="text-2xl">Wind</p>
+            <p className="font-bold text-xl">
+              {data.wind.speed.toFixed(0)} MPH{" "}
+            </p>
+          </div>
+          <div>
+            <p className="text-2xl">Coord</p>
+            <p className="font-bold text-xl">
+              {data.coord.lon},{data.coord.lat}
+            </p>
+          </div>
+          <div>
+            <p className="text-2xl">Description</p>
+            <p className="font-bold text-xl">{data.weather[0].description}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,13 @@ export default function Home() {
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   const fetchWeather = (e) => {
     e.preventDefault();
     setLoading(true);
     axios.get(url).then((response) => {
       setWeather(response.data);
-      // console.log(response.data);
     });
     setCity("");
     setLoading(false);
